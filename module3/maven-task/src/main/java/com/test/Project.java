14f4e950c9d1a7efab9a03ceab95594b8daaeceb
vashkevich <7855862@gmail.com>
2017-02-27 20:51:28 +0300

    test2

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -11,49 +11,18 @@ class Project {
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
-  }
-class Project {
-
-  public static void test() {
-    System.out.println("test");
-  }
-  
-  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
+    System.out.println("Hellow MTN");
     System.out.println("Hellow MTN");
   }
-
-}
-  class Project3 {
-
-  public static void test() {
-    System.out.println("test");
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("Hellow MTN");
-  }
-
-}
-  class Project4 {
-
-  public static void test() {
-    System.out.println("test");
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("Hellow MTN");
-  }
-
-}
-  class Project {
-
-  public static void test() {
-    System.out.println("test");
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("Hellow MTN");
-  }
-
-}
 }
