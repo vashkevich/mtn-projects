8794ef9cfa2f2ab473dc162c9619e7ab8de95bf5
vashkevich <7855862@gmail.com>
2017-02-27 20:49:28 +0300

    test

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -12,5 +12,48 @@ class Project {
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
+class Project {
 
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
+  class Project3 {
+
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
+  class Project4 {
+
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
+  class Project {
+
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
 }
